082e965ecd2b8b880a3e432ad75f58de6de883bf
Antonio Neto <antonio.bengelsdorff@gmail.com>
2016-11-15 11:21:14 +0000

    Update Bindings section Add static index.html to development proposes Add npm script start

--- Angular2-Playground.SPA/ClientApp/app/components/bindings/bindings.component.ts
+++ Angular2-Playground.SPA/ClientApp/app/components/bindings/bindings.component.ts
@@ -2,12 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'bindings',
-    template: require('./bindings.component.html')
+    template: require('./bindings.component.html'),
+    styles: [require('./bindings.component.css')]
 })
 export class BindingsComponent {
     public currentCount = 0;
+    public isSpecial = true;
+    public isSelected = false;
+    public imageUrl = 'http://ofbiz-vm2.apache.org:28080/images/products/GZ-1000/small.png';
 
-    public incrementCounter() {
+    public incrementCounter(event: Event ) {
+        event.preventDefault();
         this.currentCount++;
     }
+
+    public decrementCounter(event: Event) {
+        event.preventDefault();
+        this.currentCount--;
+    }
+    public switchIsSpecial(event: Event) {
+        event.preventDefault();
+        this.isSpecial = !this.isSpecial;
+    }
+
+    
 }
