11c3a99414849cb66c709f57a86fb5d28cd66166
Antonio Neto <antonio.bengelsdorff@gmail.com>
2016-11-17 00:02:54 +0000

    Add page header

--- Angular2-Playground.SPA/ClientApp/app/components/githubcorner/githubcorner.component.ts
+++ Angular2-Playground.SPA/ClientApp/app/components/githubcorner/githubcorner.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'github-corner',
-    inputs: ['href'],
     template: require('./githubcorner.component.html'),
     styles: [require('./githubcorner.component.css')]
 })
 export class GitHubCornerComponent implements OnInit {
-
-    public href: string;
+    @Input() href: string;
     constructor() {
     }
 
