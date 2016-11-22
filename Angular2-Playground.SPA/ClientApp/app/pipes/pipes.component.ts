375bd5715f0327692410cd4b3a07ea9c6fc6e8db
Antonio Neto <antonio.bengelsdorff@gmail.com>
2016-11-22 18:21:45 +0000

    Add pipes component Fix csproj compile group

--- Angular2-Playground.SPA/ClientApp/app/pipes/pipes.component.ts
+++ Angular2-Playground.SPA/ClientApp/app/pipes/pipes.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
     template: require('./pipes.component.html')
 })
 export class PipesComponent {
+    simpleText: string = 'This is a text';
+    piNumber: number = 3.141592;
+    clubsList: Array<string> = ['Benfica', 'Porto', 'Sporting'];
+    todayDate: number = Date.now();
+
 }
