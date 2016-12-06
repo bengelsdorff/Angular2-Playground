3fe839759924fa6f67d1081b53807f616e2a9cc3
Antonio Neto <antonio.bengelsdorff@gmail.com>
2016-12-06 17:43:53 +0000

    Update template driven form

--- Angular2-Playground.SPA/ClientApp/app/templatedrivenforms/templatedrivenforms.component.ts
+++ Angular2-Playground.SPA/ClientApp/app/templatedrivenforms/templatedrivenforms.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'template-driven-forms',
     template: require('./templatedrivenforms.component.html')
 })
-export class TemplateDrivenFormsComponent {
+export class TemplateDrivenFormsComponent implements OnInit {
+     template: any;
+
+    ngOnInit() { }
+
+    onSubmit(template) {
+        this.template = template;
+    }
 }
